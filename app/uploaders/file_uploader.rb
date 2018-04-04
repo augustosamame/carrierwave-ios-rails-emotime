828e98e2d6f5fd1dd8a1da3915b4df7faa33a9e3
Augusto Samame <augustosamame@gmail.com>
2018-04-04 15:30:05 -0500

    removed resize to 200

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -3,7 +3,7 @@
 class FileUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [200, 200]
+  #process resize_to_fit: [200, 200]
 
   version :thumb do
     process resize_to_fill: [40,40]
